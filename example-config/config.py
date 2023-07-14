f667738c7a28982dfc3afc19e1d03441f15277e2
Wolfgang Meier <womeier@posteo.de>
2023-07-14 23:18:08 +0200

    add example config

---
+++ example-config/config.py
@@ -0,0 +1,15 @@
+# CONFIG
+
+# GITHUB
+# ========================================================
+# needs read-access to: Administration, Contents, Metadata
+GITHUB_TOKEN = "github_pat_SECRET"
+GITHUB_USERNAME = "johndoe"
+
+
+# GITOLITE
+# ========================================================
+# key needs read access to repos
+GITOLITE_URL = "git.example.org"
+GITOLITE_USER = "git"
+GITOLITE_PRIVKEY = "/home/johndoe/.ssh/id_ed25519"
